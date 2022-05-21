fdcb9c70d8bede420d2a47b94883c2ec49c3f6b8
BatturM <battur_maidar@brown.edu>
2022-05-21 14:17:09 +0800

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './Main';
+import Navbar from './Navbar';
+import data from './data';
 
-function App() {
+export default function App() {
+
+  const datas = data.map(items => {
+    return (
+      <Main
+        country={items.country}
+        img={items.img}
+        link={items.link}/>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {datas}
     </div>
   );
 }
 
-export default App;
+
